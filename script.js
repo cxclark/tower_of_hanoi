18762112a316aded623e2694943b0ea1906a1451
carolinexc <caroline@humanperspectives.com>
2022-04-30 14:06:40 -0400

    game logic

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const towerC = document.querySelector('#towerC');
 
 // Instante a variable to track when there is a click
 // This will help with the logic of moving pieces
-let active = false;
+let nodeSelected = false;
 
 // Create discs using DOM elements
 function createDiscs() {
@@ -57,8 +57,16 @@ function createDiscs() {
 }
 createDiscs();
 
+// This holds the main logic of the game
 let gameplay = function(){
-    // This holds the main logic of the game
+    // If first click and clicking no disks, return
+    if (nodeSelected === false && this.childElementCount === 0) {
+        return
+    // If you click on the same tower twice, de-select the disc
+    } else if (nodeSelected === this.firstElementChild) {
+        this.firstElementChild.style.borderWidth = '1px';
+        nodeSelected = false;
+    }
     // On first click, if the tower has a disc(s), the highlight the smallest disk
         // If there are no discs on that tower, do nothing
     // On second click, check to see if the move is legal
@@ -66,8 +74,48 @@ let gameplay = function(){
         // If the move is valid, move the disc
 }
 
-// Instantiate event listeners on the towers
-towers.addEventListener('click', gameplay)
+
+
+
+
+
+
+
+
+// Instantiate event listeners on the towers, which delegates to the individual tower
+towers.addEventListener('click', (e) => {
+    console.log(e.target);
+    // If there hasn't been a first click and you click on an empty tower, return
+    if (active === false && e.target.childElementCount === 0) {
+        return
+    // If there hasn't been a first click and you click on a non-empty tower
+    } else if (active === false && e.target.childElementCount !== 0) {
+        e.target.firstElementChild.style.borderWidth = '10px';
+        active = true;
+        selectedNode = e.target.firstElementChild
+    // If there has been a first click and you click on an empty tower, move the disk
+    } else if (selectedNode.offsetWidth < e.target.firstElementChild.offsetWidth || e.target.childElementCount === 0) {
+        selectedNode.style.borderWidth = '1px';
+        e.target.appendChild(selectedNode);
+        active = false;
+    }
+    // stop delegation to children
+    // easier to remove the ability for event to be one of the disks
+    // try event listeniners on the individual towers
+    // OR stop delegation to the children so it only ever gets tagged to the towers
+});
+
+// Stop event propagation to to the discs, so the tower stays selected 
+discs = document.querySelectorAll('.disc');
+discs.forEach(disc => {
+    disc.addEventListener('click', (e) => {
+        e.stopPropagation();
+    });
+});
+
+// towerA.addEventListener('click', (e) => {
+//     e.stopPropagation();
+// })
 
 
 // Create a function to check if the game as been won
